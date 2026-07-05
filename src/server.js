42f7b7ff60bf3f80a075b01480d6c86ed19188f7
Ashraful-IR <rifo0118@gmail.com>
2026-07-05 11:55:36 +0600

    Al setup including DB connection is done succesfully

---
+++ src/server.js
@@ -0,0 +1,22 @@
+import dotenv from "dotenv/config.js";
+import app from "./app.js";
+import pool from "./config/dbconfig.js";
+const PORT = process.env.PORT || 5000;
+
+async function startServer() {
+  try {
+    // Test database connection
+    await pool.query("SELECT 1");
+
+    console.log("Database connected successfully");
+
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+  } catch (error) {
+    console.error("Failed to connect to database");
+    console.error(error.message);
+  }
+}
+
+startServer();
